5ffdfc72625cd3e32171514547d8626a393050e6
2nyyy <24Merano@gmail.com>
2024-02-20 14:16:14 +0800

    commit index

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey Questionaire</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+
 <style>
 * {
   box-sizing: border-box;
@@ -32,12 +38,12 @@ input {
   border: 1px solid #aaaaaa;
 }
 
-/* Mark input boxes that gets an error on validation: */
+
 input.invalid {
   background-color: #ffdddd;
 }
 
-/* Hide all steps by default: */
+
 .tab {
   display: none;
 }
@@ -58,9 +64,11 @@ button:hover {
 
 #prevBtn {
   background-color: #bbbbbb;
+  color: #fff;
+  
 }
 
-/* Make circles that indicate the steps of the form: */
+
 .step {
   height: 15px;
   width: 15px;
@@ -76,50 +84,132 @@ button:hover {
   opacity: 1;
 }
 
-/* Mark the steps that are finished and valid: */
+
 .step.finish {
   background-color: #04AA6D;
+}
+
+h1 {
+  margin-bottom: 20px;
 }
 </style>
 <body>
 
 <form id="regForm" action="/action_page.php">
-  <h1>Questions</h1>
-  <h2>1</h2>
-  <div class="tab">Name:
+
+  <div class="tab">
+  <h1 style="font-weight: bold;">Register:</h1>
     <p><input placeholder="First name..." oninput="this.className = ''" name="fname"></p>
     <p><input placeholder="Last name..." oninput="this.className = ''" name="lname"></p>
   </div>
-  <div class="tab">Contact Info:
-    <p><input placeholder="E-mail..." oninput="this.className = ''" name="email"></p>
-    <p><input placeholder="Phone..." oninput="this.className = ''" name="phone"></p>
+  <div class="tab">Question 1: <br><br>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, et?</p><br><br>
+    <div class="form-check-inline">
+      <label class="form-check-label" for="radio">
+        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1">&nbsp;&nbsp;Option 1 <br><br>
+        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">&nbsp;&nbsp;Option 2<br><br>
+        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option3">&nbsp;&nbsp;Option 3<br><br>
+        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option4">&nbsp;&nbsp;Option 4
+      </label>
+    </div>
+    <div id="error-message"></div>
   </div>
-  <div class="tab">Birthday:
-    <p><input placeholder="dd" oninput="this.className = ''" name="dd"></p>
-    <p><input placeholder="mm" oninput="this.className = ''" name="nn"></p>
-    <p><input placeholder="yyyy" oninput="this.className = ''" name="yyyy"></p>
+  <div class="tab">Question 2: <br><br>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, et?</p><br><br>
+    <div class="form-check-inline">
+      <label class="form-check-label" for="radio">
+        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1">&nbsp;&nbsp;Option 1 <br><br>
+        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">&nbsp;&nbsp;Option 2<br><br>
+        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option3">&nbsp;&nbsp;Option 3<br><br>
+        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option4">&nbsp;&nbsp;Option 4
+      </label>
+    </div> 
   </div>
-  <div class="tab">Login Info:
-    <p><input placeholder="Username..." oninput="this.className = ''" name="uname"></p>
-    <p><input placeholder="Password..." oninput="this.className = ''" name="pword" type="password"></p>
-  </div>
-  <div style="overflow:auto;">
-    <div style="float:right;">
-      <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-      <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+  <div class="tab">Question 3: <br><br>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, et?</p><br><br>
+    <div class="form-check-inline">
+      <label class="form-check-label" for="radio">
+        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1">&nbsp;&nbsp;Option 1 <br><br>
+        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">&nbsp;&nbsp;Option 2<br><br>
+        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option3">&nbsp;&nbsp;Option 3<br><br>
+        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option4">&nbsp;&nbsp;Option 4
+      </label>
     </div>
   </div>
-  <!-- Circles which indicates the steps of the form: -->
+  <div class="tab">Question 4: <br><br>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, et?</p><br><br>
+    <div class="form-check-inline">
+      <label class="form-check-label" for="radio">
+        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1">&nbsp;&nbsp;Option 1 <br><br>
+        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">&nbsp;&nbsp;Option 2<br><br>
+        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option3">&nbsp;&nbsp;Option 3<br><br>
+        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option4">&nbsp;&nbsp;Option 4
+      </label>
+    </div>
+  </div>
+  <div class="tab">Question 5: <br><br>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, et?</p><br><br>
+    <div class="form-check-inline">
+      <label class="form-check-label" for="radio">
+        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1">&nbsp;&nbsp;Option 1 <br><br>
+        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">&nbsp;&nbsp;Option 2<br><br>
+        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option3">&nbsp;&nbsp;Option 3<br><br>
+        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option4">&nbsp;&nbsp;Option 4
+      </label>
+    </div>
+  </div>
+  <div style="overflow:auto;"><br><br>
+    <div style="text-align:center;">
+      <button type="button" class="btn btn-md " id="prevBtn" onclick="nextPrev(-1)">Previous</button>&nbsp;&nbsp;
+      <button type="button" class="btn btn-primary" id="nextBtn" onclick="nextPrev(1)">Next</button>
+    </div>
+  </div>
+
   <div style="text-align:center;margin-top:40px;">
     <span class="step"></span>
     <span class="step"></span>
     <span class="step"></span>
     <span class="step"></span>
-  </div>
+    <span class="step"></span>
+    <span class="step"></span>
+</div>
 </form>
 
+
+
+
+
+
+
+
 <script>
-var currentTab = 1; // Current tab is set to be the first tab (0)
+
+function nextPrev() {
+  var radios = document.getElementsByName("optradio");
+  var formValid = false;
+
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      formValid = true;
+      break;
+    }
+  }
+
+  if (!formValid) {
+    var errorMessage = document.getElementById("error-message");
+    errorMessage.textContent = "Please select an option.";
+  } else {
+    errorMessage.textContent = ""; // Clear any previous error message
+    nextPrev(1); // Proceed to the next step
+  }
+}
+
+function nextPrev(n) {
+  // Function to handle navigation between steps, you can implement it according to your requirements
+}
+
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
@@ -127,20 +217,7 @@ function showTab(n) {
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   //... and fix the Previous/Next buttons:
-  if (currentTab >= x.length) {
-    // ... the form gets submitted:
-    document.getElementById("regForm").submit();
-    return false;
-}
-if (currentTab >= x.length) {
-    // ... the form gets submitted:
-    // Redirect to the desired page or category
-    window.location.href = "/your-next-page"; // Change "/your-next-page" to the desired URL
-    return false;
-}
-
-
-  if (n == 1) {
+  if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
     document.getElementById("prevBtn").style.display = "inline";
@@ -205,6 +282,7 @@ function fixStepIndicator(n) {
   x[n].className += " active";
 }
 </script>
+
 
 </body>
 </html>
